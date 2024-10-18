6b7d24bc2b2bfbd6e8724c50be45a23c70adc6be
Alex Kondratenko <kopernik@inbox.ru>
2024-10-18 11:46:18 +0300

    6.1

--- functions.js
+++ functions.js
@@ -130,7 +130,7 @@ async function GetDate(dateInput) {
     const month = String(date.getMonth() + 1).padStart(2, '0'); // Months are zero-indexed
     const day = String(date.getDate()).padStart(2, '0');
     //return `${year}-${month}-${day}`;
-    return `${year}
+    return `${year}`;
 }
 
 function formatDate(date) {
